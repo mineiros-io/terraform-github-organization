ba384156d5e7c7fba564b5f630ccc9d5612894e8
Marius Tolzmann <m@rius.berlin>
2020-01-12 18:31:54 +0100

    Add projects as output and add description for outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "memberships" {
-  value = github_membership.membership
+  description = "A map of members and admins keyed by username."
+  value       = github_membership.membership
+}
+
+output "projects" {
+  description = "A map of projects keyed by the id (default: project name)."
+  value       = github_organization_project.project
 }
